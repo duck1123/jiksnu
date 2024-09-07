17c4077d25ee142a10b74715967faa6bc035a523
Duck Nebuchadnezzar <duck@kronkltd.net>
2024-09-07 09:32:59 -0400

    bump clojure deps

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :resource-paths ["resources" "target/resources" "node_modules"]
   :test-paths ["test" "test-cljs" "specs"]
   :cucumber-glue-paths ["specs"]
-  :dependencies [[cider/cider-nrepl "0.15.0"]
+  :dependencies [[cider/cider-nrepl "0.50.2"]
                  [ciste "0.6.0-SNAPSHOT"
                   :exclusions [ring/ring-core
                                org.clojure/clojure
@@ -18,38 +18,42 @@
                                xerces/xercesImpl]]
                  [ciste/ciste-incubator "0.1.0-20170109.012825-2" :exclusions [ciste ciste/ciste-core]]
                  [clj-factory "0.2.2-SNAPSHOT"]
-                 [clj-time "0.14.0"]
-                 [clj-http "3.6.1"]
-                 [compojure "1.6.0"]
+                 [clj-time "0.15.2"]
+                 [clj-http "3.13.0"]
+                 [compojure "1.7.1"]
                  [com.cemerick/friend "0.2.3"]
                  [com.getsentry.raven/raven "8.0.3"
                   :exclusions [org.slf4j/slf4j-api]]
-                 [com.novemberain/monger "3.1.0" :exclusions [com.google.guava/guava]]
-                 [com.novemberain/validateur "2.5.0"]
-                 [com.taoensso/timbre "4.10.0"]
-                 [crypto-random "1.2.0"]
+                 [com.novemberain/monger "3.6.0" :exclusions [com.google.guava/guava]]
+                 [com.novemberain/validateur "2.6.0"]
+                 [com.taoensso/timbre "6.5.0"]
+                 [crypto-random "1.2.1"]
                  [hiccup "1.0.5"]
                  [hiccups "0.3.0"]
-                 [liberator "0.15.1"]
-                 [manifold "0.1.6"]
-                 [mvxcvi/puget "1.0.1"]
+                 ;; https://mvnrepository.com/artifact/jakarta.xml.bind/jakarta.xml.bind-api
+                 [jakarta.xml.bind/jakarta.xml.bind-api "4.0.2"]
+                 [liberator "0.15.3"]
+                 [manifold "0.4.3"]
+                 [mvxcvi/puget "1.3.4"]
                  [net.kronkltd/clj-gravatar "0.1.0-20120321.005702-1"]
                  [net.kronkltd/octohipster "0.3.0-20151001.045924-2"
                   :exclusions [inflections]]
-                 [org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/clojurescript "1.9.671"]
-                 [org.clojure/core.async "0.3.443"
+                 [org.clojure/clojure "1.11.4"]
+                 [org.clojure/clojurescript "1.11.132"]
+                 [org.clojure/core.async "1.6.681"
                   :exclusions [org.clojure/tools.reader org.clojure/core.cache]]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [org.clojure/tools.reader "1.0.3"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.slf4j/slf4j-log4j12 "1.7.25"]
-                 [ring "1.6.2"]
-                 [ring/ring-defaults "0.3.0"]
+                 [org.clojure/tools.logging "1.3.0"]
+                 [org.clojure/tools.reader "1.5.0"]
+                 [org.clojure/data.json "2.5.0"]
+                 ;; https://mvnrepository.com/artifact/org.glassfish.jaxb/jaxb-runtime
+                 [org.glassfish.jaxb/jaxb-runtime "4.0.5"]
+                 [org.slf4j/slf4j-log4j12 "2.0.16"]
+                 [ring "1.12.2"]
+                 [ring/ring-defaults "0.5.0"]
                  [slingshot "0.12.2"]]
   :cljfmt {:indents {#".*fact.*" [[:inner 0]]}}
   :plugins [[codox "0.8.13" :exclusions [org.clojure/clojure]]
-            [lein-ancient "0.6.10"]
+            [lein-ancient "0.7.0"]
             [lein-annotations "0.1.0" :exclusions [org.clojure/clojure]]
             [lein-checkall "0.1.1" :exclusions [org.clojure/tools.namespace org.clojure/clojure]]
             [lein-cljfmt "0.5.2"  :exclusions [org.clojure/clojure
@@ -105,28 +109,27 @@
                                  ;; :verbose true
                                  :pretty-print true}}}}
   :profiles {:dev {:dependencies
-                   [[midje "1.9.0-alpha5" :exclusions [org.clojure/clojure]]
-                    [figwheel-sidecar "0.5.11"
+                   [[midje "1.10.10" :exclusions [org.clojure/clojure]]
+                    [figwheel-sidecar "0.5.20"
                      :exclusions [http-kit org.clojure/core.cache]]
-                    [org.clojure/test.check "0.9.0"]
+                    [org.clojure/test.check "1.1.1"]
                     [org.clojure/tools.nrepl "0.2.13"]
                     [ring-mock "0.1.5"]
                     [clj-webdriver "0.7.2"]
-                    [org.seleniumhq.selenium/selenium-server "3.4.0"
+                    [org.seleniumhq.selenium/selenium-server "3.141.59"
                      :exclusions [org.eclipse.jetty/jetty-io
                                   org.eclipse.jetty/jetty-util
                                   net.java.dev.jna/jna xerces/xercesImpl
                                   org.seleniumhq.selenium/selenium-support
                                   org.seleniumhq.selenium/selenium-api]]
-                    [org.seleniumhq.selenium/selenium-api "3.4.0"]
-                    [org.seleniumhq.selenium/selenium-support "3.4.0"]
+                    [org.seleniumhq.selenium/selenium-api "4.24.0"]
+                    [org.seleniumhq.selenium/selenium-support "4.24.0"]
                     [slamhound "1.5.5"]]}
              :e2e {:cljsbuild {:builds
                                {:protractor
                                 {:source-paths ["specs-cljs"]
                                  :notify-command ["notify-send"]
-                                 :compiler {
-                                            :output-to "target/protractor-tests.js"
+                                 :compiler {:output-to "target/protractor-tests.js"
                                             ;; :output-dir "target/specs/"
                                             :optimizations :simple
                                             :target :nodejs
